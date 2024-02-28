848498632b6338b36bebf7e327e0c8069bc42daf
hristovskii <petarhristovski23@gmail.com>
2024-02-28 02:55:56 +0100

    added events to the landing page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { ReactElement } from "react";
+import EventsShowcase from "../components/events-showcase";
 import BlogShowcase from "../components/blog-showcase";
 import LandingPage from "../components/landing-page";
 import UnauthenticatedLayout from "../components/layout/unauthenticated-layout";
@@ -44,6 +45,8 @@ const Home = () => {
         <WhyYouShouldJoin />
 
         <MakingMemories />
+
+        <EventsShowcase />
 
         <BlogShowcase />
 
