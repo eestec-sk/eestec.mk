a8162393d74327cd4633b1268d9c395bdfdbd704
Petar Hristovski <petarhristovski23@gmail.com>
2024-08-01 22:33:39 +0200

    something idk

--- frontend/src/sections/JoinUsSectionComponent.tsx
+++ frontend/src/sections/JoinUsSectionComponent.tsx
@@ -1,28 +1,60 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import JoinUsComponent from "../components/JoinUsComponent";
 
-import Img from '../images/logos_icons/events.svg'
-import Img2 from '../images/logos_icons/meeting people.svg'
-import Img3 from '../images/logos_icons/development.svg'
+import Img from "../images/logos_icons/events.svg";
+import Img2 from "../images/logos_icons/meeting people.svg";
+import Img3 from "../images/logos_icons/development.svg";
 import SubheadingComponent from "@/components/SubheadingComponent";
 
+import HeroImage from "../images/testing/testing.png";
+
 const JoinUsSectionComponent = () => {
-    return(
-        <div className="m-2 grid justify-items-center">
-            <div><SubheadingComponent text="Why Should you Join us?"/>
-            </div>
-            <div className="grid justify-self-start">
-              <JoinUsComponent img={Img} heading={"International and local events"} text={"Be part of the organizing committee or simply attend an international workshop, exchange or motivational weekend. You will certainly not regret it!"}></JoinUsComponent>
-            </div>
-            <div className="grid justify-self-center">
-                <JoinUsComponent img={Img2} heading={"Meeting new people"} text={"You will have the chance to meet new people from all over Europe and explore their culture, tradition and cuisine."}></JoinUsComponent>
-            </div>
-            <div className="grid justify-self-end">
-               <JoinUsComponent img={Img3} heading={"Personal development"} text={"Improve your skills and qualities through training and set life goals that will improve your employment prospects."}></JoinUsComponent>
-            </div>
+  return (
+    <div
+      className="w-full p-10"
+      style={{
+        backgroundImage: `url(${HeroImage.src})`,
+        backgroundPosition: `center`,
+        backgroundSize: `cover`,
+        backgroundRepeat: `no-repeat`,
+       
+      }}
+    >
+      <div className="m-2 grid justify-items-center">
+        <div>
+          <SubheadingComponent text="Why Should you Join us?" />
         </div>
-    );
-}
+        <div className="grid justify-self-start ">
+          <JoinUsComponent
+            img={Img}
+            heading={"International and local events"}
+            text={
+              "Be part of the organizing committee or simply attend an international workshop, exchange or motivational weekend. You will certainly not regret it!"
+            }
+          ></JoinUsComponent>
+        </div>
+        <div className="grid justify-self-center">
+          <JoinUsComponent
+            img={Img2}
+            heading={"Meeting new people"}
+            text={
+              "You will have the chance to meet new people from all over Europe and explore their culture, tradition and cuisine."
+            }
+          ></JoinUsComponent>
+        </div>
+        <div className="grid justify-self-end">
+          <JoinUsComponent
+            img={Img3}
+            heading={"Personal development"}
+            text={
+              "Improve your skills and qualities through training and set life goals that will improve your employment prospects."
+            }
+          ></JoinUsComponent>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default JoinUsSectionComponent;
