af40ba1774181e936736ece84d9c07925791f60f
Petar Hristovski <petarhristovski23@gmail.com>
2024-09-04 14:11:53 +0200

    adding new component for memories section

--- frontend/src/sections/MemoriesComponent.tsx
+++ frontend/src/sections/MemoriesComponent.tsx
@@ -6,8 +6,7 @@ import Img3 from '../images/memories/memory3.jpg'
 import Img4 from '../images/memories/memory4.jpg'
 import Img5 from '../images/memories/memory5.jpg'
 import Img6 from '../images/memories/memory6.jpg'
-import Img7 from '../images/memories/memory1.jpg'
-import Img8 from '../images/memories/memory1.jpg'
+import MemoriesImagesComponent from "@/components/MemoriesImagesComponent";
 
 const MemoriesComponent = () => {
     return(
@@ -17,15 +16,15 @@ const MemoriesComponent = () => {
         <h3 className="text-black text-2xl text-bold text-center">MAKING MEMORIES SINCE 2003</h3>
         </div>
         <div className="lg:flex justify-center items-center gap-20 p-3">
-            <Image src={Img} alt="Sponsor Logo" style={{border: "5px solid #e52a30"  }} height={500}  width={500} className="mt-8 mb-8 "></Image>
-            <Image src={Img2} alt="Sponsor Logo" style={{border: "5px solid #e52a30" }} height={500} width={500} className="mt-8 mb-8 "></Image>
-            <Image src={Img3} alt="Sponsor Logo" style={{border: "5px solid #e52a30" }} height={500} width={500} className="mt-8 mb-8 "></Image>
+        <MemoriesImagesComponent img={Img.src} text={"test"}></MemoriesImagesComponent>
+        <MemoriesImagesComponent img={Img2.src} text={"test"}></MemoriesImagesComponent>
+        <MemoriesImagesComponent img={Img3.src} text={"test"}></MemoriesImagesComponent>
         </div>
 
         <div className="lg:flex justify-center items-center gap-20 p-3">
-            <Image src={Img4} alt="Sponsor Logo" style={{border: "5px solid #e52a30" }} height={500} width={500} className="mt-8 mb-8 "></Image>
-            <Image src={Img5} alt="Sponsor Logo" style={{border: "5px solid #e52a30" }} height={500} width={500} className="mt-8 mb-8 "></Image>
-            <Image src={Img6} alt="Sponsor Logo" style={{border: "5px solid #e52a30" }} height={500} width={500} className="mt-8 mb-8 "></Image>
+        <MemoriesImagesComponent img={Img4.src} text={"test"}></MemoriesImagesComponent>
+        <MemoriesImagesComponent img={Img5.src} text={"test"}></MemoriesImagesComponent>
+        <MemoriesImagesComponent img={Img6.src} text={"test"}></MemoriesImagesComponent>
         </div>
     </section>
     );
