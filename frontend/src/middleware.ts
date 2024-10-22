6c3702992723c51a7135b756722da4962e30cfe6
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-10-22 22:38:53 +0200

    Added forgotten password to pages you can't access while logged in

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/login', '/register'],
+    matcher: ['/login', '/register', '/forgotten-password'],
 };
